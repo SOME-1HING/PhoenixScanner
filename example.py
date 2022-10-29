fc02f236de699ffdb5493a0e64219647c4cfdee8
SOME1HING <yashprakash2005@gmail.com>
2022-10-30 01:16:24 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+[In reply to SOME1HING]
 import os
 from pyrogram import Client, filters
 from pyrogram.types import Message
@@ -16,15 +17,15 @@ async def red7xphoenix(bot: RedSeven, message: Message):
    if check['is_gban']:
       user = await bot.get_users(user.id)
       msg = f"""
-** Alert ⚠️**
+ Alert ⚠️
 User [{user.first_name}](tg://user?id={user.id}) is officially
 Scanned by Team Red7 | Phoenix API ;)
 
 Appeal [Here](https://t.me/Red7WatchSupport)
 """
       try:
-         await bot.ban_chat_member(user.id)
-         await bot.send_message(chat.id, msg)
+         await bot.ban_chat_member(chat.id, user.id)
+         await bot.send_message(chat.id, msg, disable_web_page_preview=True)
       except:
-         await bot.send_message(chat.id, msg)
+         await bot.send_message(chat.id, msg, disable_web_page_preview=True)
          pass
