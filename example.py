ca492003b30361e0fc84f41b6612af947ee7919d
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2022-10-31 23:05:04 +0530

    Done 🌝

--- example.py
+++ example.py
@@ -18,7 +18,7 @@ async def update_list():
 loop = get_event_loop() 
 loop.create_task(update_list())
    
-@RedSeven.on_message(filters.group & filters.all)
+@RedSeven.on_message(filters.user(SCANLIST) & filters.group)
 async def red7xphoenix(bot: RedSeven, message: Message):
    user = message.from_user
    chat = message.chat
