d79dd8f1af12b82f77b68907e21d290e4b7fbfab
SOME1HING <yashprakash2005@gmail.com>
2022-11-01 14:55:58 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,22 +1,24 @@
 import os
-import asyncio
-from pyrogram import Client, filters
-from pyrogram.types import Message
 from PhoenixScanner import Phoenix
 from .. import pbot as RedSeven 
+from pyrogram.types import Message
+from pyrogram import filters
 
 RED = Phoenix(os.getenv("RED7_TOKEN"))
 SCANLIST = []
 
-async def update_list():
-   global SCANLIST
-   newlist = RED.scanlist()
-   SCANLIST = newlist
-   await asyncio.sleep(60)
+@RedSeven.on_message(filters.command("updatescan") & filters.user(5483120234))
+async def update_list(bot: RedSeven, message: Message):
+    global SCANLIST
+    newlist = RED.scanlist()
+    if newlist == {'message': 'Invalid Token'}:
+        newlist = RED.scanlist()
+        if newlist == {'message': 'Invalid Token'}:
+            raise 'Invalid Scanner Token'
+    SCANLIST = newlist
+    await message.reply_text(f"Scanlist Updated.\nScanned users: `{SCANLIST}`")
+    
 
-loop = asyncio.get_event_loop() 
-loop.create_task(update_list())
-   
 @RedSeven.on_message(filters.group & filters.all)
 async def red7xphoenix(bot: RedSeven, message: Message):
    user = message.from_user
@@ -28,7 +30,6 @@ async def red7xphoenix(bot: RedSeven, message: Message):
  Alert ⚠️
 User [{user.first_name}](tg://user?id={user.id}) is officially
 Scanned by Team Red7 | Phoenix API ;)
-
 Appeal [Here](https://t.me/Red7WatchSupport)
 """
       try:
@@ -37,5 +38,4 @@ Appeal [Here](https://t.me/Red7WatchSupport)
       except:
          await bot.send_message(chat.id, msg, disable_web_page_preview=True)
          pass
-
- 
+     
